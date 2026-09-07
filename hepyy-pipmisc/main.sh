5ee62272da2acb4dfd71ab6e50f9ff6b5ab5d36c
matplo <ploskon@gmail.com>
2026-09-06 22:33:23 -0700

    pip install a few things

--- hepyy-pipmisc/main.sh
+++ hepyy-pipmisc/main.sh
@@ -1,3 +1,7 @@
 set -e
 
-python3 -m pip install ipykernel jupyter_client nbconvert seaborn plotly ipywidgets matplotlib mdedit diskuh datafileviewer -y
+python3 -m pip install ipykernel jupyter_client nbconvert seaborn plotly ipywidgets matplotlib
+
+python3 -m pip install mdedit diskuh datafileviewer
+
+python3 -m pip install scipaperlib
