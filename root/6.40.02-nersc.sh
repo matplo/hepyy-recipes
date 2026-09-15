0bce3c66ef67093913883c6cba85b80c8523860e
matplo <noksolp@gmail.com>
2026-09-14 22:57:39 -0700

    for nersc

---
+++ root/6.40.02-nersc.sh
@@ -0,0 +1,17 @@
+set -e
+build_dir={{ srcdir }}-build
+mkdir -p "$build_dir"
+cd "$build_dir"
+cmake {{ srcdir }} \
+  -DCMAKE_INSTALL_PREFIX={{ prefix }} \
+  -DCMAKE_BUILD_TYPE=Release \
+  -DPython3_EXECUTABLE=$(which python3) \
+  -Dbuiltin_xrootd=OFF \
+  -Dxrootd=OFF \
+  -Dvmc=OFF \
+  -Dmathmore=ON \
+  -Dxml=ON \
+  -Dunfold=ON \
+  -Dbuiltin_vdt=ON \
+  -Dbuiltin_gif=ON
+cmake --build . --target install -- -j {{ n_cores }}
