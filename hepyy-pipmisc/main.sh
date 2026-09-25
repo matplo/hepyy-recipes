70ed6d66b25f03e63ff2381134c893a890cc206e
matplo <ploskon@gmail.com>
2026-09-24 22:37:48 -0700

    Add installation of hepyy-workflows package

--- hepyy-pipmisc/main.sh
+++ hepyy-pipmisc/main.sh
@@ -1,5 +1,7 @@
 set -e
 
+python3 -m pip install hepyy-workflows
+
 python3 -m pip install ipykernel jupyter_client nbconvert seaborn plotly ipywidgets matplotlib
 
 python3 -m pip install mdedit diskuh datafileviewer
