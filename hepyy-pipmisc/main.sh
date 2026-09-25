42d855b5520e9caf2e18569b435fec8709af2d9e
matplo <ploskon@gmail.com>
2026-09-24 22:38:40 -0700

    Add pip-check-resolve to installation list

--- hepyy-pipmisc/main.sh
+++ hepyy-pipmisc/main.sh
@@ -4,6 +4,6 @@ python3 -m pip install hepyy-workflows
 
 python3 -m pip install ipykernel jupyter_client nbconvert seaborn plotly ipywidgets matplotlib
 
-python3 -m pip install mdedit diskuh datafileviewer
+python3 -m pip install mdedit diskuh datafileviewer pip-check-resolve
 
 python3 -m pip install scipaperlib
